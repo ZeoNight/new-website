a5454fafe0964fc06f6168f343d1a87870e83fdb
ZeoNight <me@zeonight.com>
2022-07-26 18:24:32 +0300

    Create funny.js

---
+++ tests/funny.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/j40p3_YduIM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
