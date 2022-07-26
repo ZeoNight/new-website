29bcb200fbdba768d5b8f074fde5046d7e9bfc22
ZeoNight <me@zeonight.com>
2022-07-26 23:57:02 +0300

    Update funny.js

--- tests/funny.js
+++ tests/funny.js
@@ -1,1 +1,12 @@
-document.body.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/j40p3_YduIM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+document.body.innerHTML = `<iframe id="video" src="https://www.youtube.com/embed/j40p3_YduIM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" width="2140" height="1000" frameborder="0"></iframe>`
+let elem = document.getElementById("video");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+openFullScreen();
